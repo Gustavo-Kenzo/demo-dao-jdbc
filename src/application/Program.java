c286340248004c03e19fff74712b4b3f367cef44
Gustavo <sgkenzo212007@gmail.com>
2025-06-12 17:57:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		Department dp = new Department(1,"computers");
-		System.out.println(dp);
+		Seller seller = new Seller(1,"Kenzo", "kenzo@gmail.com", LocalDate.parse("22/04/2007",sdf),2500.00,dp);
+		System.out.println(seller);
 
 	}
 
