ea0d1536989c11d0179181516a90d4aef7b11b2a
Gustavo <sgkenzo212007@gmail.com>
2025-06-13 16:24:04 -0300

    DaoFactory and SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,17 @@
 package application;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import model.entities.Department;
-import model.entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
 	public static void main(String[] args) {
-		DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		Department dp = new Department(1,"computers");
-		Seller seller = new Seller(1,"Kenzo", "kenzo@gmail.com", LocalDate.parse("22/04/2007",sdf),2500.00,dp);
-		System.out.println(seller);
+//		DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//		Department dp = new Department(1, "computers");
+//		Seller seller = new Seller(1, "Kenzo", "kenzo@gmail.com", LocalDate.parse("22/04/2007", sdf), 2500.00, dp);
+//		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createDaoSeller();
+		
 
 	}
 
