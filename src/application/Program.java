0f4fda0f51d935ef532c89a23d745cefa2ea012c
Gustavo <sgkenzo212007@gmail.com>
2025-06-13 22:19:47 -0300

    Implemented findById; fixed db.properties

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,13 +6,9 @@ import model.dao.SellerDao;
 public class Program {
 
 	public static void main(String[] args) {
-//		DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//		Department dp = new Department(1, "computers");
-//		Seller seller = new Seller(1, "Kenzo", "kenzo@gmail.com", LocalDate.parse("22/04/2007", sdf), 2500.00, dp);
-//		System.out.println(seller);
-		SellerDao sellerDao = DaoFactory.createDaoSeller();
+		SellerDao sd = DaoFactory.createDaoSeller();
+		System.out.println(sd.findById(3));
 		
-
 	}
 
 }
